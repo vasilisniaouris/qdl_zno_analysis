a067333e932ed7db1d8a88847ab3b8b1ec4717c9
vasilisniaouris <vasilisniaouris@gmail.com>
2023-05-15 18:19:31 -0700

    Updated QuantityDataFrame to print quantity array elements.

--- qdl_zno_analysis/data_utils/quantity_dataframes.py
+++ qdl_zno_analysis/data_utils/quantity_dataframes.py
@@ -1,7 +1,10 @@
+from io import StringIO
 
 import numpy as np
+import pint
 import pint_pandas
 import pandas as pd
+from pandas.io.formats import format as fmt
 
 from qdl_zno_analysis import Qty
 
@@ -34,3 +37,22 @@ class QuantityDataFrame(pd.DataFrame):
 
         super().__setitem__(key, value)
 
+    def __repr__(self) -> str:
+        """
+        Return a string representation for a particular DataFrame. Modified cause PintArray can not properly display
+        2D arrays.
+        """
+
+        def formatting_function(quantity):
+            return "{:{float_format}}".format(
+                getattr(quantity, 'magnitude', quantity), float_format=getattr(quantity, 'default_format', '')
+            )
+
+        if self._info_repr():
+            buf = StringIO()
+            self.info(buf=buf)
+            return buf.getvalue()
+
+        repr_params = fmt.get_dataframe_repr_params()
+        repr_params['formatters'] = [formatting_function] * len(self.columns)
+        return self.to_string(**repr_params)
